6d9ce0e85bf30a78cb9d1e5eec736e55767fbfdd
josegallo <josegallo@gmail.com>
2019-04-14 23:24:34 +0200

    adding index and signup php and views

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+if (isset($_SESSION['user'])) {
+    header('location: contenido.php');
+} else {
+    header('location: signup.php');
+}
+
+?>
---
+++ views/signup.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chraset= "UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, 
+    initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title> Sign Up </title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Sign up</h1>
+        <hr class="border">
+
+    </div>
+</body>
+</html>
