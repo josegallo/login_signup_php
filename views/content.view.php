4d6d5a3798052145001a9235a086e4f79aa7adbd
josegallo <josegallo@gmail.com>
2019-04-14 22:58:51 +0200

    2nd try

---
+++ views/content.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chraset= "UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, 
+    initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title> Content </title>
+</head>
+<body>
+    <div class="container">
+    <h1 class="title">Web Content</h1>
+    <a href="close.php">Close Session</a>
+        <div class="content"><article>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Ut, numquam dicta labore velit alias necessitatibus perferendis temporibus! 
+                Dicta quam velit quidem magnam modi, totam tenetur possimus ex qui fugiat natus.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Ut, numquam dicta labore velit alias necessitatibus perferendis temporibus! 
+                Dicta quam velit quidem magnam modi, totam tenetur possimus ex qui fugiat natus.
+            </p>
+        </article>
+        </div>
+    </div>
+</body>
+</html>
